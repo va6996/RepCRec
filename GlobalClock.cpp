847b288de2574911329754027806b9f04b76a0c1
Vinayak Agarwal <va2083@nyu.edu>
2021-12-04 12:25:23 -0500

    Add a global clock

---
+++ GlobalClock.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Vinayak Agarwal on 12/4/21.
+//
+
+#include "GlobalClock.h"
+
+GlobalClock::time = 0;
---
+++ GlobalClock.h
@@ -0,0 +1,23 @@
+//
+// Created by Vinayak Agarwal on 12/4/21.
+//
+
+#ifndef REPCREC_GLOBALCLOCK_H
+#define REPCREC_GLOBALCLOCK_H
+
+
+class GlobalClock {
+	static int time;
+
+public:
+		void tick() {
+			time++;
+		}
+
+		static int getTime() {
+			return time;
+		}
+};
+
+
+#endif //REPCREC_GLOBALCLOCK_H
