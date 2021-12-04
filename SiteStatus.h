449e02e4d64bfa77aa0d394febed62236cb330a3
Vinayak Agarwal <va2083@nyu.edu>
2021-12-04 12:25:41 -0500

    Add site status enum

---
+++ SiteStatus.h
@@ -0,0 +1,14 @@
+//
+// Created by Vinayak Agarwal on 12/4/21.
+//
+
+#ifndef REPCREC_SITESTATUS_H
+#define REPCREC_SITESTATUS_H
+
+
+enum SiteStatus {
+	Up, Down
+};
+
+
+#endif //REPCREC_SITESTATUS_H
